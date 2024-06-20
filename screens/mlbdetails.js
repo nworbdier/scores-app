8aeb076c82477462f89c7121bcc108ca9ba1fc48
Reid Brown <reidbrown23@gmail.com>
2024-06-20 14:54:07 -0400

    Updates

--- screens/mlbdetails.js
+++ screens/mlbdetails.js
@@ -553,7 +553,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   basesContainer: {
-    marginVertical: 15,
+    marginVertical: 10,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
