ea167cbe1c312083cc199a97a3827ccd5128130f
Reid Brown <reidbrown23@gmail.com>
2024-06-26 17:43:20 -0400

    Updates

--- screens/nhldetails.js
+++ screens/nhldetails.js
@@ -136,7 +136,13 @@ const NHLDetails = ({ route }) => {
               <ScrollView>
                 {filteredItems.map((item) => (
                   <View key={item.index} style={styles.feedItem}>
-                    <Text style={styles.feedItemText}>{item.text}</Text>
+                    <Text
+                      style={[
+                        styles.feedItemText,
+                        { fontWeight: item.scoringPlay ? 'bold' : 'normal' },
+                      ]}>
+                      {item.text}
+                    </Text>
                   </View>
                 ))}
               </ScrollView>
